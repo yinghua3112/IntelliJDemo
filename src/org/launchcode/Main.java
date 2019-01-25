290cade76228d57bc5a91627b8baf1c870c5f9cb
Ying FANG(Frannie) <frannie.fang.ying@gmail.com>
2019-01-25 11:18:24 -0500

    readd the change

--- src/org/launchcode/Main.java
+++ src/org/launchcode/Main.java
@@ -4,6 +4,11 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("Hello Please ffpp");
+        System.out.println("Hello Please");
+
+            System.out.println("Hello WHOOld");
+
+            System.out.println("Welcome to wonderland");
+
     }
 }
